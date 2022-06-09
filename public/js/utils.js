5e3f33bb6911e131454ec9bcbb5a1c2fc4e565ac
Ognjen <radisicognjen@gmail.com>
2022-06-09 16:32:25 +0200

    Add simultanious movement for both players

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,36 +1,36 @@
-function movePlayer(e) {
-	let player1 = this.getNode("player-1");
-	let player2 = this.getNode("player-2");
+function movePlayer(controls) {
+	let player1 = app.getNode("player-1");
+	let player2 = app.getNode("player-2");
 
 	//If W is Clicked then player1 will move in left direction
-	if (e.keyCode == 87) {
+	if (controls.player1Up) {
 		player1.y = player1.y > 0 ? player1.y - player1.height / 12 : player1.y;
 		if (player1.y < 0) player1.y = 0;
 	}
 
 	//If S is Clicked then player2 will move in Right direction
-	if (e.keyCode == 83) {
+	if (controls.player1Down) {
 		player1.y =
-			player1.y + player1.height < this.height
+			player1.y + player1.height < app.height
 				? player1.y + player1.height / 12
 				: player1.y;
-		if (player1.y + player1.height > this.height)
-			player1.y = this.height - player1.height;
+		if (player1.y + player1.height > app.height)
+			player1.y = app.height - player1.height;
 	}
 
 	//If Up Arrow is Clicked then player2 will go up
-	if (e.keyCode == 38) {
+	if (controls.player2Up) {
 		player2.y = player2.y > 0 ? player2.y - player2.height / 12 : player2.y;
 		if (player2.y < 0) player2.y = 0;
 	}
 
 	//If Down Arrow is Clicked then player2 will move down
-	if (e.keyCode == 40) {
+	if (controls.player2Down) {
 		player2.y =
-			player2.y + player2.height < this.height
+			player2.y + player2.height < app.height
 				? player2.y + player2.height / 12
 				: player2.y;
-		if (player2.y + player2.height > this.height)
-			player2.y = this.height - player2.height;
+		if (player2.y + player2.height > app.height)
+			player2.y = app.height - player2.height;
 	}
 }
